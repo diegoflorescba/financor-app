49bd2c31cd48ea0177b434c2be622124ee77b7d6
Diego Flores <diego.flores@helloglobo.com>
2025-02-25 18:15:22 -0300

    actualizado run backup

--- run_backup_panywhere.sh
+++ run_backup_panywhere.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/diegoaflores/financor-app
-/home/diegoaflores/.virtualenvs/prestamos_env/bin/python backup_db.py >> /home/diegoaflores/financor-app/instance/backup.log 2>&1
+python backup_db.py >> /home/diegoaflores/financor-app/instance/backup.log 2>&1
